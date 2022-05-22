149606218ce2b1cc131b622920c8a264bc2740fb
Your email <email@example.com>
2022-05-22 17:28:23 +0800

    Modify Icon

---
+++ app/src/main/java/com/example/neudatemanager/AddScheduleActivity.java
@@ -0,0 +1,174 @@
+package com.example.neudatemanager;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.neudatemanager.entity.Schedule;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+public class AddScheduleActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_schedule);
+
+        //寻找组件
+        imageView =findViewById(R.id.imageView_Commit);
+        listView = findViewById(R.id.ListView_AddSchedule);
+        imageView = findViewById(R.id.imageView_Commit);
+
+        //获取creator
+        SharedPreferences sharedPreferences = getSharedPreferences(null,MODE_PRIVATE);
+        String creator = sharedPreferences.getString("creator",null);
+
+        Schedule schedule = new Schedule(creator);
+
+        //初步设置list
+        setListView(schedule);
+
+        //编辑日程
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0://设置标题
+                        final EditText inputServer = new EditText(AddScheduleActivity.this);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(AddScheduleActivity.this);
+                        builder.setTitle("输入标题")
+                                .setIcon(android.R.drawable.ic_dialog_info)
+                                .setView(inputServer)
+                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                });
+                        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                schedule.setName(inputServer.getText().toString());
+                                dialogInterface.dismiss();
+                                setListView(schedule);
+                            }
+                        });
+                        builder.show();
+                        break;
+                    case 1://设置日期
+                        Calendar calendar = Calendar.getInstance();
+                        new DatePickerDialog(AddScheduleActivity.this, new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                Date date = new Date(year-1900,month,day);
+                                schedule.setDay(schedule.formatDate(date));
+                                setListView(schedule);
+                            }
+                        },calendar.get(Calendar.YEAR)
+                        ,calendar.get(Calendar.MONTH)
+                        ,calendar.get(Calendar.DAY_OF_MONTH)).show();
+                        break;
+                    case 2://设置开始时间
+                        calendar = Calendar.getInstance();
+                        new TimePickerDialog(AddScheduleActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                                Date date = new Date();
+                                date.setHours(hourOfDay);
+                                date.setMinutes(minute);
+                                schedule.setStartTime(schedule.formatTime(date));
+                                setListView(schedule);
+                            }
+                        },calendar.get(Calendar.HOUR_OF_DAY)
+                        ,calendar.get(Calendar.MINUTE)
+                        ,true).show();
+                        break;
+                    case 3://设置结束时间
+                        calendar = Calendar.getInstance();
+                        new TimePickerDialog(AddScheduleActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                                Date date = new Date();
+                                date.setHours(hourOfDay);
+                                date.setMinutes(minute);
+                                schedule.setEndTime(schedule.formatTime(date));
+                                setListView(schedule);
+                            }
+                        },calendar.get(Calendar.HOUR_OF_DAY)
+                                ,calendar.get(Calendar.MINUTE)
+                                ,true).show();
+                        break;
+                }
+            }
+        });
+
+        //提交按钮
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long isSuccess = schedule.write(null,AddScheduleActivity.this);
+                if(isSuccess == -1){
+                    Toast.makeText(AddScheduleActivity.this,"创建失败",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(AddScheduleActivity.this,"创建成功",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(AddScheduleActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                //清空数据库用
+//                schedule.emptyDB(null,AddScheduleActivity.this);
+            }
+        });
+    }
+
+    //list设置函数
+    public void setListView(Schedule schedule){
+        ArrayList list = new ArrayList();
+        HashMap map  = new HashMap();
+
+        map.put("head","事件标题");
+        map.put("body",schedule.getName());
+        list.add(map);
+        map = new HashMap();
+
+        map.put("head","事件日期");
+        map.put("body",schedule.getDay());
+        list.add(map);
+        map = new HashMap();
+
+        map.put("head","开始时间");
+        map.put("body",schedule.getStartTime());
+        list.add(map);
+        map = new HashMap();
+
+        map.put("head","结束时间");
+        map.put("body",schedule.getEndTime());
+        list.add(map);
+
+        String[] from = {"head","body"};
+        int[] to = {R.id.textView_Head,R.id.textView_Body};
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,list,R.layout.list_add_schedule,from,to);
+        listView.setAdapter(simpleAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/neudatemanager/entity/Schedule.java
@@ -0,0 +1,126 @@
+package com.example.neudatemanager.entity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.neudatemanager.sqlite.DBOpenHelper;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Schedule {
+    private String name;
+    private String startTime;
+    private String endTime;
+    private String creator;
+    private String day;
+
+
+    public Schedule(String creator) {
+        this.creator = creator;
+    }
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public Schedule() {
+    }
+
+    public Schedule(String name,String startTime,String endTime,String creator,String day) {
+        this.name = name;
+        this.creator =creator;
+        this.startTime = startTime;
+        this.day = day;
+        this.endTime = endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator =creator;
+    }
+
+
+    //写入Schedule信息
+    public long write(String nullColumnHack,Context context){
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context,"schedule.db",null,1);
+        SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name",this.getName());
+        contentValues.put("startTime",this.getStartTime());
+        contentValues.put("endTime",this.getEndTime());
+        contentValues.put("creator",this.getCreator());
+        contentValues.put("day",this.getDay());
+        return db.insert("schedule",nullColumnHack,contentValues);
+    }
+
+    //返回该日期下的数据库的指针
+    public Cursor getCursorByDay(String nullColumnHack,Context context,String day){
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context,"schedule.db",null,1);
+        SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+        Cursor cursor = db.query("schedule",new String[]{"name as _id","startTime","endTime"},"day = ? and creator = ?",new String[]{day,this.creator},null,null,null);
+        return cursor;
+    }
+
+    //格式化日期
+    public String formatDate(Date date){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String formatDate  = simpleDateFormat.format(date);
+        return formatDate;
+    }
+
+    //格式化时间
+    public String formatTime(Date date){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        String formatDate  = simpleDateFormat.format(date);
+        return formatDate;
+    }
+    //获取指针
+    public Cursor getCursor(String nullColumnHack,Context context){
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context,"schedule.db",null,1);
+        SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+        Cursor cursor = db.query("schedule",new String[]{"name","startTime","endTime","creator","day"},"name = ?",new String[]{this.creator},null,null,null);
+
+        return cursor;
+    }
+
+    //清空数据库用
+    public void emptyDB(String nullColumnHack, Context context){
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context,"schedule.db",null,1);
+        SQLiteDatabase db = dbOpenHelper.getWritableDatabase();
+        db.delete("schedule",null,null);
+    }
+}
